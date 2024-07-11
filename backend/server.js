0bc7961f17b7687ffb94fb0fc5e46d9832c82fbc
Mohamed-kassab-96 <111517134+Mohamed-kassab-96@users.noreply.github.com>
2024-07-11 21:56:24 +0300

    1- auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
-// import cookieParser from "cookie-parser";
 // import bodyParser from "body-parser";
 
 dotenv.config();
@@ -10,20 +11,12 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+// app.use(express.json({ limit: "15mb" }));
+app.use(express.urlencoded({ extended: true}));
+// app.use(express.urlencoded({ extended: true, limit: "160kb"}));
+app.use(cookieParser());
  
 app.use("/api/auth", authRoutes);
-// app.use(express.json({ limit: "15mb" }));
-// app.use(express.urlencoded({ extended: true}));
-// app.use(express.urlencoded({ extended: true, limit: "160kb"}));
-
-// app.use(express.urlencoded({ extended: true}));
-// app.use(bodyParser.json());
-// app.use(bodyParser.json({ limit: "15mb" }));
-// app.use(bodyParser.urlencoded({ extended: true, limit: "160kb"}));
-
-
-
-// app.use(cookieParser());
 
 // app.get('/', (req, res) => {
 //     res.send("js hyuedc he")
